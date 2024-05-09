626062210306df9ddb88c3a5586e1e2e158e4140
vidur <vidur.punj@hotmail.com>
2024-05-09 07:36:59 +0530

    changed layout

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,16 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets
+//= link_tree ../stylesheets/css
+//= link_tree ../stylesheets/fonts
+//= link css/tiny-slider.css
+//= link css/tobii.min.css
+//= link css/materialdesignicons.min.css
+//= link bootstrap.bundle.min.js
+//= link tiny-slider.js
+//= link tobii.min.js
+//= link feather.min.js
+//= link plugins.init.js
+//= link app.js
+//= link quill.js
 //= link bootstrap.min.js
 //= link popper.min.js
